fde33b8c9a8e795e5ea1313ceffb45a0b795c85e
kameda <you@example.com>
2016-01-29 08:10:26 +0900

    add typescript test

--- src/typescript/array2d.ts
+++ src/typescript/array2d.ts
@@ -2,37 +2,37 @@
 
 
 
-// module.exports = class Array2D{
-//
-//     private array2d;
-//
-//     constructor(row, col, value){
-//         array2d = [];
-//         for(var i = 0; i < row; i++){
-//             var array = [];
-//             for(var j = 0; j < col; j++){
-//                 array.push(value);
-//             }
-//         }
-//         array2d.push(array);
-//     }
-//
-//     public get(y, x){
-//         if(array2d[y] == undefined){
-//             return undefined;
-//         }
-//         return array2d[y][x];
-//     }
-//
-//     public set(y, x, value){
-//         if(array2d[y] == undefined){
-//             return;
-//         }
-//         array2d[y][x] = value;
-//     }
-//
-//     // reset: (value) ->
-//     //
-//     // clone: ->
-//
-// }
+export = class Array2D{
+
+    private array2d;
+
+    constructor(row, col, value){
+        this.array2d = [];
+        for(var i = 0; i < row; i++){
+            var array = [];
+            for(var j = 0; j < col; j++){
+                array.push(value);
+            }
+            this.array2d.push(array);
+        }
+    }
+
+    public get(y, x){
+        if(this.array2d[y] == undefined){
+            return undefined;
+        }
+        return this.array2d[y][x];
+    }
+
+    public set(y, x, value): void{
+        if(this.array2d[y] == undefined){
+            return;
+        }
+        this.array2d[y][x] = value;
+    }
+
+    // reset: (value) ->
+    //
+    // clone: ->
+
+}
