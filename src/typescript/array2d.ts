684d9c5fe6773d3eb43e17f1c918c16a6096a7f5
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-01-29 07:25:00 +0900

    add typescript

---
+++ src/typescript/array2d.ts
@@ -0,0 +1,38 @@
+
+
+
+
+// module.exports = class Array2D{
+//
+//     private array2d;
+//
+//     constructor(row, col, value){
+//         array2d = [];
+//         for(var i = 0; i < row; i++){
+//             var array = [];
+//             for(var j = 0; j < col; j++){
+//                 array.push(value);
+//             }
+//         }
+//         array2d.push(array);
+//     }
+//
+//     public get(y, x){
+//         if(array2d[y] == undefined){
+//             return undefined;
+//         }
+//         return array2d[y][x];
+//     }
+//
+//     public set(y, x, value){
+//         if(array2d[y] == undefined){
+//             return;
+//         }
+//         array2d[y][x] = value;
+//     }
+//
+//     // reset: (value) ->
+//     //
+//     // clone: ->
+//
+// }
